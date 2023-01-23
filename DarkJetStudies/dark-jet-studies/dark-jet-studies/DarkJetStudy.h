c98b4402b15e11e5e90a5cbeb5173d8a21c21b89
GustavLindberg99 <95423695+GustavLindberg99@users.noreply.github.com>
2023-01-23 15:07:09 +0100

    Add files via upload

--- DarkJetStudies/dark-jet-studies/dark-jet-studies/DarkJetStudy.h
+++ DarkJetStudies/dark-jet-studies/dark-jet-studies/DarkJetStudy.h
@@ -23,11 +23,20 @@ private:
 
     int _numEvents;
 
+    std::vector<double> _efficiencyData;
     std::map<int, int> _jetMultiplicityData;    //Contains the number of jets with pT > 30GeV as key, and the number of events with that key as value
     std::map<int, int> _darkJetMultiplicity20Data, _darkJetMultiplicity50Data, _darkJetMultiplicity80Data;    //Same as above but only counts jets with darkness > 20% / 50% / 80%
+    double _responseSum;
+    int _numberOfEventsWithResponse;
+    std::map<int, double> _responseSumByInvisibility, _responseSumByDarkness;
+    std::map<int, int> _numberOfEventsByInvisibility, _numberOfEventsByDarkness;
 
     static constexpr int _bins = 50;
     static constexpr double _maxPT = 3e3;
+    static constexpr int _deltaRBins = 12;
+    static constexpr double _deltaRMax = 0.6;
+    static constexpr double _maxResponse = 2.0;
     TH1 *_leadingJetPTPlot, *_subLeadingJetPTPlot, *_thirdLeadingJetPTPlot, *_dijetInvariantMassPlot;
     TH1 *_leadingJetInvisiblePlot, *_subLeadingJetInvisiblePlot, *_thirdLeadingJetInvisiblePlot, *_leadingJetDarknessPlot, *_subLeadingJetDarknessPlot, *_thirdLeadingJetDarknessPlot;
+    TH1 *_responsePlot, *_leadingEfficiencyPlot, *_subLeadingEfficiencyPlot, *_thirdLeadingEfficiencyPlot;
 };
