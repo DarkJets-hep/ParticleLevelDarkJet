20de3098b79d0b06ead880269592d0a8732648a5
GustavLindberg99 <95423695+GustavLindberg99@users.noreply.github.com>
2023-03-07 12:26:53 +0100

    Add files via upload

--- Headers/ParticleName.hpp
+++ Headers/ParticleName.hpp
@@ -155,11 +155,14 @@ static std::string particleName(int pdgid){
         case 4900021: return "DARK_GLUON";
         case 4900022: return "DARK_PHOTON";
         case 4900023: return "ZPRIMEBOSON";
-        case 4900101: return "DARK_QUARK";
+        case -4900101: return "DARK_QUARK";
+        case 4900101: return "DARK_ANTIQUARK";
         case 4900111: return "DARK_PION";
         case 4900113: return "DARK_RHO_MESON";
-        case 4900211: return "DARK_KAON";
-        case 4900213: return "DARK_KSTAR";
+        case -4900211: return "DARK_KAON";
+        case 4900211: return "DARK_KAON_BAR";
+        case -4900213: return "DARK_KSTAR";
+        case 4900213: return "DARK_KSTAR_BAR";
     }
 
     if(pdgid < 0){
@@ -310,11 +313,14 @@ static std::string particleNameAsTLatex(int pdgid){
         case 4900021: return "g_{D}";
         case 4900022: return "#gamma'";
         case 4900023: return "Z'";
-        case 4900101: return "q_{D}";
+        case -4900101: return "q_{D}";
+        case 4900101: return "#bar{q}_{D}";
         case 4900111: return "#pi_{D}";
         case 4900113: return "#rho_{D}";
-        case 4900211: return "K_{D}";
-        case 4900213: return "K*_{D}";
+        case -4900211: return "K_{D}";
+        case 4900211: return "#bar{K}_{D}";
+        case -4900213: return "K*_{D}";
+        case 4900213: return "#bar{K}*_{D}";
     }
 
     if(pdgid < 0){
